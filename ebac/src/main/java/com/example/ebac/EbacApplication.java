4eccad9dca425df6c6e1dcb8dc333a2535fbebee
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2025-11-24 22:48:55 -0300

    Migrate project to Spring Boot and remove old code
    Removed previous car factory implementation and related source files. Added new Spring Boot project under 'ebac' directory with Maven configuration, application and test classes, and entity 'Produto'. Updated project structure and configuration files to support the new setup.

---
+++ ebac/src/main/java/com/example/ebac/EbacApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.example.ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbacApplication.class, args);
+	}
+
+}
