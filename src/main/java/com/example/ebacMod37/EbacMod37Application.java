5fb704fea6dfd2edc4a6d29576e8ef0464ec89f2
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2025-11-29 11:42:37 -0300

    mod37

---
+++ src/main/java/com/example/ebacMod37/EbacMod37Application.java
@@ -0,0 +1,13 @@
+package com.example.ebacMod37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbacMod37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbacMod37Application.class, args);
+	}
+
+}
