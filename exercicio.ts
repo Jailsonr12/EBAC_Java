e8e5476a40d9adcf6470ee4437bb74c636d7f13c
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-01-16 15:40:13 -0300

    Update exercicio.ts

--- exercicio.ts
+++ exercicio.ts
@@ -1,10 +1,10 @@
-export function multiplicar(a: number, b: number): number {
+function multiplicar(a: number, b: number): number {
     return a * b;
 }
 
-export function saudacao(nome: string): string {
+function saudacao(nome: string): string {
     return `Olá ${nome}`;
 }
 
-console.log(multiplicar(3, 4)); // 12
-console.log(saudacao("Jailson")); // Olá Jailson
+console.log(multiplicar(3, 4));
+console.log(saudacao("Jailson"));
